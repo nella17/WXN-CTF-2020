950ffc68f9ef22d188a67282de3a5c33877c7d7c
Allen Guan <tsguan11@gmail.com>
2020-02-24 12:18:49 +0800

    Psychic-Py Solved

---
+++ Psychic-Py/topic.py
@@ -0,0 +1,18 @@
+# uncompyle6 version 3.6.2
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 3.7.3 (default, Mar 27 2019, 16:54:48)
+# [Clang 4.0.1 (tags/RELEASE_401/final)]
+# Embedded file name: /mnt/c/Users/user/Desktop/final/misc/C_f1le/topic.py
+# Compiled at: 2020-02-19 13:55:04
+from gmpy2 import *
+from Crypto.Util.number import *
+from libnum import *
+from pwn import *
+from hashlib import *
+from Crypto.PublicKey.RSA import *
+from WXN import H3re_
+Is_a = bytes_to_long(H3re_)
+e = 1
+c = 1600991603
+n = 3582547033594242656407334311363221999813223
+# okay decompiling topic.pyc
